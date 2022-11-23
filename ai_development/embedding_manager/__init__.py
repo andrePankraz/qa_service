f32ef27724bf58b6692513103d6855083813a94c
andrePankraz <andre@pankraz.de>
2022-11-23 17:48:02 +0100

    Just remember for now

---
+++ ai_development/embedding_manager/__init__.py
@@ -0,0 +1,7 @@
+'''
+This file was created by ]init[ AG 2022.
+
+Package for Model "No Language Left Behind" (NLLB).
+'''
+from .nllb_manager import NllbManager
+from .nllb_languages import LANGUAGES
